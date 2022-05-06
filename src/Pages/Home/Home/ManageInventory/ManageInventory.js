b9bfaf6113ef8b63c713f63c5511daee76af0a8c
Habibur137 <infernohabib@gmail.com>
2022-05-06 14:03:51 +0600

    manage inventory system implemented

--- src/Pages/Home/Home/ManageInventory/ManageInventory.js
+++ src/Pages/Home/Home/ManageInventory/ManageInventory.js
@@ -1,9 +1,38 @@
 import React from "react";
+import useInventory from "../../../../hooks/useInventory";
+import ManageItem from "../../ManageItem/ManageItem";
 
 const ManageInventory = () => {
+  const [inventory, setInventory] = useInventory();
+  console.log(inventory);
+
   return (
-    <div>
-      <h1>this is manage inventory</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-lg-8">
+          <table className="table table-hover table-success">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Supplier</th>
+                <th scope="col">Price</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {inventory.map((item, index) => (
+                <ManageItem
+                  item={item}
+                  key={index}
+                  setInventory={setInventory}
+                  inventory={inventory}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Home/ManageItem/ManageItem.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const ManageItem = ({ item, setInventory, inventory }) => {
+  const { name, price, supplier, quantity, _id } = item;
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are you sure?");
+    if (proceed) {
+      fetch(`http://localhost:5000/inventory/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const filterInventory = inventory.filter((item) => item._id !== id);
+          setInventory(filterInventory);
+        });
+    }
+  };
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{supplier}</td>
+      <td>{price}</td>
+      <td>{quantity}</td>
+      <button
+        onClick={() => handleDelete(_id)}
+        className="bg-info border-0 w-100"
+      >
+        X
+      </button>
+    </tr>
+  );
+};
+
+export default ManageItem;
